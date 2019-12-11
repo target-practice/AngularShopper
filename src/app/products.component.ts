4218ad967588f8a873e93430c8e2ef6549e84f79
Robin Hood <fordarnold@gmail.com>
2019-12-11 11:43:25 +0200

    Change indentation from 2 spaces to 4 spaces
    Who likes 2-space indents, honestly?

--- src/app/products.component.ts
+++ src/app/products.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
-	selector: 'products', // creates custom HTML element <products>
-	template: '<h2>Products List</h2>' // the HTML markup to be rendered
+    selector: 'products', // creates custom HTML element <products>
+    template: '<h2>Products List</h2>' // the HTML markup to be rendered
 })
 export class ProductsComponent {
-	
-	// constructor(argument) {
-	// 	// code...
-	// }
+    
+    // constructor(argument) {
+    //  // code...
+    // }
 }
