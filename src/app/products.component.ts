d15e0e9a67553cbcf92a35c0aae15690c22935ed
Robin Hood <fordarnold@gmail.com>
2019-12-11 12:07:07 +0200

    Extend our simple Templates, and take them to the next level
    Data binding (using getter/setter methods) ~https://youtu.be/k5E2AVpwsko?t=6101

--- src/app/products.component.ts
+++ src/app/products.component.ts
@@ -2,9 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'products', // creates custom HTML element <products>
-    template: '<h2>{{ "Title: " + title }}</h2>' // the HTML markup to be rendered
+    template: '<h2>{{ getTitle() }}</h2>' // the HTML markup to be rendered
 })
 export class ProductsComponent {
     
     title = "Browse Our Products";
+
+    getTitle() {
+    	return this.title;
+    }
 }
