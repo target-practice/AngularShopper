ba4125201150e8321f607884f0e4771cc6fc12ea
Robin Hood <fordarnold@gmail.com>
2019-12-11 12:04:46 +0200

    Extend our simple Templates, and take them to the next level
    Data binding (between view & component) ~https://youtu.be/k5E2AVpwsko?t=6101

--- src/app/products.component.ts
+++ src/app/products.component.ts
@@ -2,11 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'products', // creates custom HTML element <products>
-    template: '<h2>Products List</h2>' // the HTML markup to be rendered
+    template: '<h2>{{ "Title: " + title }}</h2>' // the HTML markup to be rendered
 })
 export class ProductsComponent {
     
-    // constructor(argument) {
-    //  // code...
-    // }
+    title = "Browse Our Products";
 }
