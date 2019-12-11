d9dfb0302f1060d72beb420beaaf94e356ae647d
Robin Hood <fordarnold@gmail.com>
2019-12-11 12:19:23 +0200

    Loop through data (+ modify DOM) using an Angular concept: Directives
    Simplified: using strings instead of objects ~https://youtu.be/k5E2AVpwsko?t=6248

--- src/app/products.component.ts
+++ src/app/products.component.ts
@@ -2,11 +2,22 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'products', // creates custom HTML element <products>
-    template: '<h2>{{ getTitle() }}</h2>' // the HTML markup to be rendered
+    template: 
+    	`
+    	<h2>{{ getTitle() }}</h2>
+    	<ul>
+    		<li *ngFor="let product of products">
+    			{{ product }}
+    		</li>
+    	</ul>
+
+    	` // the HTML markup to be rendered
 })
 export class ProductsComponent {
     
     title = "Browse Our Products";
+
+    products = ["product 1", "product 2", "product 3"];
 
     getTitle() {
     	return this.title;
